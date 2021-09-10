0b319e2bbeb33642560cb7c9b167a2333e8a4223
noslav7 <noslav7@gmail.com>
2021-09-10 11:51:18 +0500

    Generics

--- src/main/java/ru/job4j/generics/GenericUsage.java
+++ src/main/java/ru/job4j/generics/GenericUsage.java
@@ -4,17 +4,16 @@ import java.util.*;
 
 public class GenericUsage {
     public static void main(String[] args) {
-        List<Person> per = List.of(new Person("name", 21, new Date(913716000000L)));
-        new GenericUsage().printInfo(per);
-
-        List<Programmer> pro = List.of(new Programmer("name123", 23, new Date(913716000000L)));
-        new GenericUsage().printInfo(pro);
+        List<? super Integer> list = new ArrayList<>();
+        new GenericUsage().addAll(list);
     }
 
-    public void printInfo(Collection<? extends Person> col) {
-        for (Iterator<? extends Person> it = col.iterator(); it.hasNext();) {
-            Person next = it.next();
-            System.out.println(next);
+    public void addAll(List<? super Integer> list) {
+        for (int i = 1; i <= 5; i++) {
+            list.add(i);
+        }
+        for (Object o : list) {
+            System.out.println("Текущий элемент: " + o);
         }
     }
 }
