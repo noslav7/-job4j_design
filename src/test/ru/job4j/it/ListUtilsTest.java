97c9852568273b63e935a89198a70d3f827af3e6
Valentin Rachkov <noslav7@gmail.com>
2022-02-06 11:10:02 +0500

    7. ListIterator [#350217]

--- src/test/ru/job4j/it/ListUtilsTest.java
+++ src/test/ru/job4j/it/ListUtilsTest.java
@@ -4,12 +4,16 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static java.lang.Math.E;
+import static java.lang.Math.multiplyExact;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class ListUtilsTest {
+
+    Predicate<Integer> predicate = i -> i == 2;
 
     @Test
     public void whenAddBefore() {
@@ -32,18 +36,26 @@ public class ListUtilsTest {
     }
 
     @Test
+    public void whenAddAfterSecond() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
+        ListUtils.addAfter(input, 1, 3);
+        assertThat(input, is(Arrays.asList(0, 1, 3, 2)));
+    }
+
+    @Test
     public void whenRemoveIfValue() {
         List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2));
-        ListUtils.removeIf(input, new List<T>(List.of(2)), 8);
-        assertThat(input, is(Arrays.asList(0, 1, 8)));
+        ListUtils.removeIf(input, predicate);
+        assertThat(input, is(Arrays.asList(0, 1, 2)));
     }
 
     @Test
     public void whenReplaceIfValue() {
         List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
-        ListUtils.replaceIf(input, new List<T>(List.of(3)), 10);
-        assertThat(input, is(Arrays.asList(0, 1, 2, 10, 4)));
+        ListUtils.replaceIf(input, predicate, 3);
+        assertThat(input, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
+
     @Test
     public void whenRemoveAllElements() {
         List<Integer> input = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5));
