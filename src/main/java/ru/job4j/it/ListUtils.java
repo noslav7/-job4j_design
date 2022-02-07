3b8bb16f0858bf46acc415026dd9bed20eeecf1c
Valentin Rachkov <noslav7@gmail.com>
2022-02-07 12:15:29 +0500

    7. ListIterator [#350217 #260844]

--- src/main/java/ru/job4j/it/ListUtils.java
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -2,16 +2,15 @@ package ru.job4j.it;
 
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator(index);
-        while (iterator.hasNext() && iterator.nextIndex() == index) {
                  iterator.add(value);
                  iterator.next();
-        }
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
@@ -20,31 +19,22 @@ public class ListUtils {
             list.add(value);
         } else {
             ListIterator<T> iterator = list.listIterator(index + 1);
-            while (iterator.hasNext() && iterator.nextIndex() == (index + 1)) {
                     iterator.add(value);
                     iterator.next();
-            }
         }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            T element = iterator.next();
-            if (element.equals(filter)) {
-                iterator.remove();
-            }
-        }
+        List<T> result = list.stream()
+                .filter(filter)
+                .toList();
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            T element = iterator.next();
-            if (element.equals(filter)) {
-                iterator.set(value);
-            }
-        }
+        List<T> result = list.stream()
+                .filter(filter)
+                .map(item -> value)
+                .toList();
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
