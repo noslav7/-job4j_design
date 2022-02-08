bd939a84e7e2a8ab01f22264d2f8b2a9f357b201
Valentin Rachkov <noslav7@gmail.com>
2022-02-08 17:38:08 +0500

    7. ListIterator [#350217 #260844]

--- src/test/ru/job4j/list/SimpleArrayListTest.java
+++ src/test/ru/job4j/list/SimpleArrayListTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
