af7d89496c6cca590b614f51f7d7f6b8f91e0bae
Valentin Rachkov <noslav7@gmail.com>
2022-02-09 12:42:26 +0500

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 public class User {
     private String name;
@@ -11,5 +13,16 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar irinaCalendar = new GregorianCalendar(1989, 4, 9);
+        Calendar vitaliyCalendar = new GregorianCalendar(2019, 4, 9);
+        User user1 = new User("Ирина", 1, irinaCalendar);
+        User user2 = new User("Виталий", 0, vitaliyCalendar);
+        HashMap<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
