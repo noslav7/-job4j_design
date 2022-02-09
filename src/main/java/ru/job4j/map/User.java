57cc14284285a9bf3b3770468b0394d5e536776d
Valentin Rachkov <noslav7@gmail.com>
2022-02-09 21:07:27 +0500

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,26 +1,36 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Objects.hash;
 
 public class User {
     private String name;
     private int children;
-    private Calendar birthday;
+    private Timestamp birthday;
 
-    public User(String name, int children, Calendar birthday) {
+    public User(String name, int children, Timestamp birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
     }
 
+    @Override
+    public int hashCode() {
+       int result = birthday.hashCode();
+       result = 31 * result + Integer.hashCode(children);
+       result = 31 * result + name.hashCode();
+       return result;
+    }
+
     public static void main(String[] args) {
-        Calendar irinaCalendar = new GregorianCalendar(1989, 4, 9);
-        Calendar vitaliyCalendar = new GregorianCalendar(2019, 4, 9);
-        User user1 = new User("Ирина", 1, irinaCalendar);
-        User user2 = new User("Виталий", 0, vitaliyCalendar);
+        Timestamp irinaStamp = new Timestamp(2021, 4, 9, 20, 10, 5, 0);
+        Timestamp vitaliyStamp = new Timestamp(2021, 4, 9, 20, 10, 5, 0);
+        User user1 = new User("Катя", 9, irinaStamp);
+        User user2 = new User("Маша", 10, vitaliyStamp);
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
