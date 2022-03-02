1aca4239afdd4526bf23a7df75adca5be8f1fa85
noslav7 <noslav7@gmail.com>
2022-03-02 08:47:14 +0500

    0.1. FileOutputStream. [#252488 #270637]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -4,8 +4,8 @@ import java.io.FileInputStream;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("C:\\projects\\job4j_design" +
-                "\\src\\main\\java\\ru\\job4j\\input.txt")) {
+        try (FileInputStream in = new FileInputStream("C:\\projects\\job4j_design"
+                + "\\src\\main\\java\\ru\\job4j\\input.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 
 public class ResultFile {
    public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("С:\\Users\\Рачков\\IdeaProjects" +
-                "\\job4j_elementary\\test\\ru\\job4j\\array\\MatrixTest.java")) {
+        try (FileOutputStream out = new FileOutputStream("С:\\Users\\Рачков\\IdeaProjects"
+                + "\\job4j_elementary\\test\\ru\\job4j\\array\\MatrixTest.java")) {
                 out.write("1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20, 5, 10, 15, 20, 25".getBytes());
                 out.write(System.lineSeparator().getBytes());
         } catch (Exception e) {
