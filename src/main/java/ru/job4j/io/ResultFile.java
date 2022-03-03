1685bdec64079367b7c9544b3f8a8632fea23f89
Valentin Rachkov <noslav7@gmail.com>
2022-03-03 16:24:46 +0500

    0.2. FileInputStream [#4898 #270921]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -3,15 +3,19 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 
 public class ResultFile {
-   public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("С:\\Users\\Рачков\\IdeaProjects"
-                + "\\job4j_design\\src\\main\\java\\ru\\job4j\\ResultFile.txt")) {
-                out.write(("1, 2, 3, 4, 5, " +
-                          "2, 4, 6, 8, 10, " +
-                          "3, 6, 9, 12, 15, " +
-                          "4, 8, 12, 16, 20, " +
-                          "5, 10, 15, 20, 25").getBytes());
-                out.write(System.lineSeparator().getBytes());
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("C:\\projects\\job4j_design"
+                + "\\src\\main\\java\\ru\\job4j\\ResultFile.txt")) {
+            out.write(("1, 2, 3, 4, 5, ").getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write(("2, 4, 6, 8, 10, ").getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write(("3, 6, 9, 12, 15, ").getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write(("4, 8, 12, 16, 20, ").getBytes());
+            out.write(System.lineSeparator().getBytes());
+            out.write(("5, 10, 15, 20, 25").getBytes());
+            out.write(System.lineSeparator().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
