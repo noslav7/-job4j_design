98c3267d88d95be7da06584f18709be52e7aa3c0
noslav7 <noslav7@gmail.com>
2022-03-03 10:50:25 +0500

    0.1. FileOutputStream. [#252488 #270637]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,8 +5,12 @@ import java.io.FileOutputStream;
 public class ResultFile {
    public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("С:\\Users\\Рачков\\IdeaProjects"
-                + "\\job4j_elementary\\test\\ru\\job4j\\array\\MatrixTest.java")) {
-                out.write("1, 2, 3, 4, 5, 2, 4, 6, 8, 10, 3, 6, 9, 12, 15, 4, 8, 12, 16, 20, 5, 10, 15, 20, 25".getBytes());
+                + "\\job4j_design\\main\\java\\ru\\job4j\\ResultFile.txt")) {
+                out.write(("1, 2, 3, 4, 5, " +
+                          "2, 4, 6, 8, 10, " +
+                          "3, 6, 9, 12, 15, " +
+                          "4, 8, 12, 16, 20, " +
+                          "5, 10, 15, 20, 25").getBytes());
                 out.write(System.lineSeparator().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
