33c9a7dcc02a04d855bad5e8253992800a751811
noslav7 <noslav7@gmail.com>
2022-03-03 11:33:15 +0500

    0.1. FileOutputStream. [#252488 #270637]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 public class ResultFile {
    public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("С:\\Users\\Рачков\\IdeaProjects"
-                + "\\job4j_design\\main\\java\\ru\\job4j\\ResultFile.txt")) {
+                + "\\job4j_design\\src\\main\\java\\ru\\job4j\\ResultFile.txt")) {
                 out.write(("1, 2, 3, 4, 5, " +
                           "2, 4, 6, 8, 10, " +
                           "3, 6, 9, 12, 15, " +
