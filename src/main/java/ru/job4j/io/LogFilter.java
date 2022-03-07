3ac049bf580b40cb536cc98e5d5f7866690dda3c
Valentin Rachkov <noslav7@gmail.com>
2022-03-07 10:04:48 +0500

    0.3. BufferedReader. [#252489]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,8 +10,8 @@ public class LogFilter {
     public List<String> filter(String file) {
         List<String> matchList = null;
         try (BufferedReader input = new BufferedReader(new FileReader(".\\data\\" + file))) {
-                matchList = input.lines().filter(s -> s.contains(" 404")).collect(Collectors.toList());
-        } catch (Exception e) {
+                matchList = input.lines().filter(s -> s.contains(" 404 ")).collect(Collectors.toList());
+        } catch (IOException e) {
          e.printStackTrace();
         }
         return matchList;
