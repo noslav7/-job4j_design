e5354a0d7118ef576dbb1fce6c39c6ae93fe98b2
Valentin Rachkov <noslav7@gmail.com>
2022-03-07 23:30:25 +0500

    2. Статистика по коллекции. [#45889 #270296]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -3,31 +3,33 @@ package ru.job4j.question;
 import java.util.*;
 
 public class Analize {
-/*
+
     public static Info diff(Set<User> previous, Set<User> current) {
         int added = 0;
-        int edited = 0;
+        int changed = 0;
         int deleted = 0;
-        Map<Integer, String> currentMap = new HashMap<>();
+        Map<Integer, String> previousMap = new HashMap<>();
         for (User user : previous) {
+            previousMap.put(user.getId(), user.getName());
+        }
+        Map<Integer, String> currentMap = new HashMap<>();
+        for (User user : current) {
             currentMap.put(user.getId(), user.getName());
         }
-        Map<Integer, String> currentMapModified = new HashMap<>();
         for (User user : previous) {
-            currentMapModified.put(user.getId(), user.getName());
-        }
-        for (User user : previous) {
-            currentMapModified.put(user.getId(), user.getName());
-            if (...)) {
-                edited++;
-            }
-            if (...) {
-                added++;
-            }
-            if (...) {
+            String name = currentMap.put(user.getId(), user.getName());
+            if (Objects.equals(name, null)) {
                 deleted++;
             }
         }
-        return new Info(added, edited, deleted);
-    }*/
+        for (User user : current) {
+            String name = previousMap.put(user.getId(), user.getName());
+            if (Objects.equals(name, null)) {
+                added++;
+            } else if (!previousMap.containsValue(name)) {
+                changed++;
+            }
+        }
+        return new Info(added, changed, deleted);
+    }
 }
