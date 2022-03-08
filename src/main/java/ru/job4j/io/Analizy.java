111cc8934b3c650bb3198d11529ad18031076807
Valentin Rachkov <noslav7@gmail.com>
2022-03-08 22:41:41 +0500

    //

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,11 +1,17 @@
 package ru.job4j.io;
 
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.stream.Stream;
 
 public class Analizy {
-    public void unavailable(String source, String target) {
-
+    /* public void unavailable(String source, String target) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            in.lines()
+                    .flatMap(line -> Stream.of(line.split("/d00")))
+                    .filter(n -> !n.startsWith()).map(word -> word + " ")
+                    .forEach(target::print);
+        }
     }
 
     public static void main(String[] args) {
@@ -15,5 +21,5 @@ public class Analizy {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
