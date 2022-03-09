03d32c70f8d44cce0f50c220e69417ed0841fe11
noslav7 <noslav7@gmail.com>
2022-03-09 10:27:09 +0500

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analizy {
-    public void unavailable(String source, String target) throws IOException {
+    public static void unavailable(String source, String target) throws IOException {
         boolean online = true;
         List<String> strings = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source));
@@ -19,7 +19,7 @@ public class Analizy {
         for (int i = 0; i < strings.size(); i = i + 2) {
             if (strings.get(i).equals("200") || strings.get(i).equals("300")) {
                 if (!online) {
-                    System.out.println(strings.get(i + 1));
+                    System.out.println(strings.get(i + 1) + "/n");
                     online = true;
                 }
             } else if (strings.get(i).equals("400") || strings.get(i).equals("500")) {
@@ -32,9 +32,8 @@ public class Analizy {
     }
 
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(".\\data\\unavailable.csv"))) {
+            Analizy.unavailable(".\\data\\unavailable.csv", ".\\data\\target.txt");
         } catch (Exception e) {
             e.printStackTrace();
         }
