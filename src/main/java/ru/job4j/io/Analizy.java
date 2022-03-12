bb06762362bbd2ee0d7435141926c7f0ecc5a965
noslav7 <noslav7@gmail.com>
2022-03-12 11:51:24 +0500

    2. Анализ доступности сервера. [#859 #273162]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,35 +7,34 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analizy {
-    public static void unavailable(String source, String target) throws IOException {
+    public static void unavailable(String source, String target) {
         boolean online = true;
         List<String> strings = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-             strings = in.lines()
+            strings = in.lines()
                     .flatMap(line -> Stream.of(line.split("\\s+")))
                     .toList();
             for (int i = 0; i < strings.size(); i = i + 2) {
-                if (strings.get(i).equals("200") || strings.get(i).equals("300")) {
+                if ("200".equals(strings.get(i)) || "300".equals(strings.get(i))) {
                     if (!online) {
                         out.write(strings.get(i + 1) + System.lineSeparator());
                         online = true;
                     }
-                } else if (strings.get(i).equals("400") || strings.get(i).equals("500")) {
+                } else if ("400".equals(strings.get(i)) || "500".equals(strings.get(i))) {
                     if (online) {
                         out.write(strings.get(i + 1) + ";");
                         online = false;
                     }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(".\\data\\unavailable.csv"))) {
-            Analizy.unavailable(".\\data\\unavailable.csv", ".\\data\\target.txt");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(new FileOutputStream(".\\data\\unavailable.csv"));
+        Analizy.unavailable(".\\data\\unavailable.csv", ".\\data\\target.txt");
     }
 }
