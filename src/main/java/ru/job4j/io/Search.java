afa2801653dc42ba1de517cebbf0f91fe690afe2
noslav7 <noslav7@gmail.com>
2022-03-13 10:53:14 +0500

    5. Валидация параметров запуска. [#246865 #274075]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,23 +1,21 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Search {
-   public static void main(String[] args) throws IOException {
-       if (args.length == 2) {
-           throw new IllegalArgumentException("Root folder or extension isnull. Usage of files .txt");
-       }
+    public static void main(String[] args) throws IOException {
+        if (args.length == 2) {
+            throw new IllegalArgumentException("Root folder or extension is null. Usage of files .txt");
+        }
         Path start = Paths.get(".");
-       if (start.isAbsolute() && start.toFile().isDirectory()) {
-               search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
-           }
-       }
+        if (!start.toFile().exists() && !start.toFile().isDirectory()) {
+            throw new InvalidPathException(start.toFile().getAbsolutePath(), "Such directory or file does not exist");
+        }
+        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+    }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
