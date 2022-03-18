bc6d7d583a1dcc0817471b62aa63ae90138522cf
noslav7 <noslav7@gmail.com>
2022-03-18 21:01:23 +0500

    3.0. Тестирование IO [#173905]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+          values.put((args[0].indexOf("-"), args[0].indexOf("=")), (args[0].indexOf("=") + 1, args[0].length() - 1));
+          values.put((args[1].indexOf("-"), args[1].indexOf("=")), (args[1].indexOf("=") + 1, args[1].length() - 1));
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
