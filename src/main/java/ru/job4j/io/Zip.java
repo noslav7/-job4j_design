2b87308cb51151917d006369d6c86469c7c82cc2
noslav7 <noslav7@gmail.com>
2022-03-24 10:32:03 +0500

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -8,7 +10,15 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File file : sources) {
+                if (!Files.isDirectory(file.toPath())) {
+                    packSingleFile(file, target);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void packSingleFile(File source, File target) {
