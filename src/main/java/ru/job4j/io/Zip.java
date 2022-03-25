bc1989097d9fb1427aac976cd6081743a624407e
noslav7 <noslav7@gmail.com>
2022-03-25 08:32:22 +0500

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,8 +1,10 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.CharBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -11,10 +13,11 @@ public class Zip {
 
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File file : sources) {
-                if (!Files.isDirectory(file.toPath())) {
-                    packSingleFile(file, target);
-                }
+            for (File source : sources) {
+               zip.putNextEntry(new ZipEntry(source.toString()));
+               try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                   zip.write(out.readAllBytes());
+               }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,5 +41,13 @@ public class Zip {
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
+        ArgsName argsName = new ArgsName();
+        try (Writer arguments = new PrintWriter(String.valueOf(zip))) {
+            for (String arg : args) {
+                arguments.write(arg);
+            }
+        } catch (IOException fnf) {
+            System.out.println("File not found");
+        }
     }
 }
