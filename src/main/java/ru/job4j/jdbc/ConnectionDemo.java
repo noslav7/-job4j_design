b10ad1efa11306a9f34900b4a51b1cb0a6f89c72
noslav7 <noslav7@gmail.com>
2022-04-12 15:35:24 +0500

    0. JDBC [#6863 #287837]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -10,39 +10,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import ru.job4j.io.Config;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        File file = new File("./app.properties");
+        Config config = new Config("./app.properties");
         Class.forName("org.postgresql.Driver");
         Map<String, String> values = new HashMap<String, String>();
-        String url = null;
-        String login = null;
-        String password = null;
-        File file = new File("./app.properties");
-        try (BufferedReader read = new BufferedReader(new FileReader(file))) {
-            for (String line = read.readLine(); line != null; line = read.readLine()) {
-                if (line.isEmpty()) {
-                    continue;
-                }
-                if (line.startsWith("#")) {
-                    continue;
-                }
-                String[] arrayLine = line.split(" = ", 2);
-                if (arrayLine.length != 2 || arrayLine[0].isEmpty() || arrayLine[1].isEmpty()) {
-                    throw new IllegalArgumentException(String.format("Invalid line: %s", line));
-                }
-                values.put(arrayLine[0], arrayLine[1]);
-                if (url.equals(null)) {
-                    url = values.get("url");
-                } else if (login.equals(null)) {
-                    login = values.get("login");
-                } else if (password.equals(null)) {
-                    password = values.get("password");
-                }
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        config.load();
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metadata = connection.getMetaData();
             System.out.println(metadata.getUserName());
