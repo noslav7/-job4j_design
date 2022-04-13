19b1126c8b6b525f795e7a4fc8cf9ac54fd22ac0
noslav7 <noslav7@gmail.com>
2022-04-13 19:25:31 +0500

    0. JDBC [#6863 #287837]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,9 +1,6 @@
 package ru.job4j.jdbc;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -19,26 +16,9 @@ public class ConnectionDemo {
         Class.forName("org.postgresql.Driver");
         Map<String, String> values = new HashMap<String, String>();
         config.load();
-        try (BufferedReader read = new BufferedReader(new FileReader(file))) {
-            for (String line = read.readLine(); line != null; line = read.readLine()) {
-                if (line.isEmpty()) {
-                    continue;
-                }
-                if (line.startsWith("#")) {
-                    continue;
-                }
-                String[] arrayLine = line.split("=", 2);
-                if (arrayLine.length != 2 || arrayLine[0].isEmpty() || arrayLine[1].isEmpty()) {
-                    throw new IllegalArgumentException(String.format("Invalid line: %s", line));
-                }
-                values.put(arrayLine[0], arrayLine[1]);
-                }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        String url = values.get("hibernate.connection.url");
-        String login = values.get("hibernate.connection.login");
-        String password = values.get("hibernate.connection.password");
+        String url = config.value("hibernate.connection.url");
+        String login = config.value("hibernate.connection.login");
+        String password = config.value("hibernate.connection.password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metadata = connection.getMetaData();
             System.out.println(metadata.getUserName());
