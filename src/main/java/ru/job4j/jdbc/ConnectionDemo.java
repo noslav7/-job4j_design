2ba65bd0bedcdd39d01f3d6c4252918ebf3ce3fc
noslav7 <noslav7@gmail.com>
2022-04-14 15:37:17 +0500

    0. JDBC [#6863 #287837]

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -4,58 +4,26 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringJoiner;
+import java.sql.DatabaseMetaData;
 
 import ru.job4j.io.Config;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         File file = new File("./app.properties");
-        Map<String, String> values = new HashMap<String, String>();
-        try (Connection connection = getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                String sql = String.format(
-                        "create table if not exist demo_table(%s, %s);",
-                        "id serial primary key",
-                        "name varchar(255"
-                );
-                statement.execute(sql);
-                System.out.println(getTableScheme(connection, "demo_table"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private static Connection getConnection() throws ClassNotFoundException, SQLException {
         Config config = new Config("./app.properties");
-        config.load();
         Class.forName("org.postgresql.Driver");
+        Map<String, String> values = new HashMap<String, String>();
+        config.load();
         String url = config.value("hibernate.connection.url");
-        String login = config.value("hibernate.connection.login");
+        String username = config.value("hibernate.connection.username");
         String password = config.value("hibernate.connection.password");
-        return DriverManager.getConnection(url, login, password);
-    }
-
-    public static String getTableScheme(Connection connection, String tableName) throws Exception {
-        var rowSeparator = "-".repeat(30).concat(System.lineSeparator());
-        var header = String.format("%-15s|%-15s%n", "NAME", "TYPE");
-        var buffer = new StringJoiner(rowSeparator, rowSeparator, rowSeparator);
-        buffer.add(header);
-        try (var statement = connection.createStatement()) {
-            var selection = statement.executeQuery(String.format(
-                    "select * from %s limit 1", tableName
-            ));
-            var metaData = selection.getMetaData();
-            for (int i = 1; i <= metaData.getColumnCount(); i++) {
-                buffer.add(String.format("%-15s|%-15s%n",
-                        metaData.getColumnName(i), metaData.getColumnTypeName(i))
-                );
-            }
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            DatabaseMetaData metadata = connection.getMetaData();
+            System.out.println(metadata.getUserName());
+            System.out.println(metadata.getURL());
         }
-        return buffer.toString();
     }
 }
