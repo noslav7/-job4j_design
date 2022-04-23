9460c8df61c1af000f8712738acf5c9c0e0b6982
noslav7 <noslav7@gmail.com>
2022-04-23 22:42:15 +0500

    6. Кодировка. [#862 #292604]

--- src/main/java/ru/job4j/io/UsageEncoding.java
+++ src/main/java/ru/job4j/io/UsageEncoding.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class UsageEncoding {
     public String readFile(String path) {
         StringBuilder builder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("WINDOWS-1251")))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path, Charset.forName("UTF-8")))) {
             br.lines().map(s -> s + System.lineSeparator()).forEach(builder::append);
         } catch (IOException e) {
             e.printStackTrace();
@@ -20,7 +20,7 @@ public class UsageEncoding {
 
     public void writeDataInFile(String path, String data) {
         try (PrintWriter pw = new PrintWriter(
-                new FileWriter(path, Charset.forName("WINDOWS-1251"), true))) {
+                new FileWriter(path, Charset.forName("UTF-8"), true))) {
             pw.println(data);
         } catch (IOException e) {
             e.printStackTrace();
