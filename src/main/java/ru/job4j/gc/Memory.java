a54ec642c7afd9a1f9b5dad8902b991c7af24c16
noslav7 <noslav7@gmail.com>
2022-04-29 11:03:35 +0500

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/Memory.java
+++ src/main/java/ru/job4j/gc/Memory.java
@@ -3,16 +3,16 @@ package ru.job4j.gc;
 public class Memory {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 527_000_000; i++) {
-            new User(15, (short) 163); /* 8 bytes (Object reference) + 4 bytes (int) + 2 bytes (short) = 14 bytes */
-            new User(5); /* 8 bytes (Object reference) + 4 bytes (int) = 12 bytes */
-            new User(170); /* 8 bytes (Object reference) + 2 bytes (short) = 10 bytes */
-            new User(); /* 8 bytes (Object reference) */
+        for (int i = 0; i < 76_000_000; i++) {
+            new User(15, (short) 163); /* 4 bytes (Object reference) + 4 bytes (int) + 2 bytes (short) = 10 bytes */
+            new User(5); /* 4 bytes (Object reference) + 4 bytes (int) = 8 bytes */
+            new User(170); /* 4 bytes (Object reference) + 2 bytes (short) = 6 bytes */
+            new User(); /* 4 bytes (Object reference) */
         }
     }
 }
 
 /*
-Currently, maximum heap size is set to 2007 Mb. Hence, creating objects worth 42 bytes overall 527_000_000 times
+Currently, maximum heap size is set to 2007 Mb. Hence, creating objects with the size 28 bytes overall 76_000_000 times
  should result in the Heap overflow and an automatic launch of GC.
  */
