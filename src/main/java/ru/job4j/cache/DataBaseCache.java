a3d5e23882c503acdf8939a9b15e19a0fd699ab9
noslav7 <noslav7@gmail.com>
2022-05-10 14:07:44 +0500

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DataBaseCache.java
@@ -0,0 +1,9 @@
+package ru.job4j.cache;
+
+public class DataBaseCache extends AbstractCache<String, String> {
+
+    @Override
+    protected String load(String key) {
+        return null;
+    }
+}
--- src/main/java/ru/job4j/cache/Emulator.java
+++ src/main/java/ru/job4j/cache/Emulator.java
@@ -1,4 +1,34 @@
 package ru.job4j.cache;
 
 public class Emulator {
+    private String cachingDir;
+
+    public Emulator() {
+    }
+
+    public Emulator(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    public String getCachingDir() {
+        return cachingDir;
+    }
+
+    public void setCachingDir(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    public static void main(String[] args) {
+      /*  String command = getTypeOfCommand()*/
+
+        Emulator emulator = new Emulator();
+
+   /*     emulator.setCachingDir(getTypeOfCommand(command))*/
+
+
+    }
+
+    public static String getTypeOfCommand(String command) {
+        return command;
+    }
 }
