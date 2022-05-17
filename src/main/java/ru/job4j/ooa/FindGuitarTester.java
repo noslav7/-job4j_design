9bb40dc9209ba7b5658073f25691bab35402eb66
noslav7 <noslav7@gmail.com>
2022-05-17 13:01:31 +0500

    OOA

---
+++ src/main/java/ru/job4j/ooa/FindGuitarTester.java
@@ -0,0 +1,34 @@
+package ru.job4j.ooa;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FindGuitarTester {
+
+    public static void main(String[] args) {
+        /* Инициализация каталога гитар */
+        Inventory inventory = new Inventory();
+        initializelnventory(inventory);
+        Guitar whatErinLikes = new Guitar("", 0, Builder.FENDER, "Stratocastor",
+                Type.ELECTRIC, Wood.ALDER, Wood.ALDER);
+        List matchinfGuitars = inventory.search(whatErinLikes);
+        if (!matchinfGuitars.isEmpty()) {
+            System.out.println("Erin, you might like this guitars:");
+            for (Iterator i = matchinfGuitars.iterator(); i.hasNext(); ) {
+                Guitar guitar = (Guitar) i.next();
+                System.out.println("We have a " +
+                        guitar.getBuilder() + " " + guitar.getModel() + " " +
+                        guitar.getType() + " guitar:\n " +
+                        guitar.getBackWood() + " back and sides,\n " +
+                        guitar.getTopWood() + " top.XnYou can have it for only $" +
+                        guitar.getPrice() + "!");
+            }
+        } else {
+            System.out.println("Sorry, Erin, we have nothing for you.");
+        }
+    }
+
+    private static void initializelnventory(Inventory inventory) {
+        /* Включение описаний гитар в каталог. */
+    }
+}
---
+++ src/main/java/ru/job4j/ooa/Type.java
@@ -0,0 +1,16 @@
+package ru.job4j.ooa;
+
+public enum Type {
+    ACOUSTIC, ELECTRIC;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case ACOUSTIC:
+                return "Acoustic";
+            case ELECTRIC:
+                return "Electric";
+        }
+        return "Type{}";
+    }
+}
