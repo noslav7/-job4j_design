ce0ef66cb65e45b7fa313890dbb305bc5e10cc81
noslav7 <noslav7@gmail.com>
2022-05-18 14:59:28 +0500

    1. Принципы Kiss, Dry и Yagni [#238813]

--- src/test/ru/job4j/kiss/MaxMinTest.java
+++ src/test/ru/job4j/kiss/MaxMinTest.java
@@ -21,12 +21,32 @@ public class MaxMinTest {
     }
 
     @Test
-    public void whenStringLengthMax() {
+    public void whenStringsMax() {
+        MaxMin maxMin = new MaxMin();
+        Comparator<String> comparator = Comparator.naturalOrder();
+        List<String> listString = new ArrayList<>(List.of("England", "Andorra", "Denmark", "China", "Belgium"));
+        String country = maxMin.max(listString, comparator);
+        String expected = "England";
+        assertEquals(expected, country);
+    }
+
+    @Test
+    public void whenIntegersMin() {
         MaxMin maxMin = new MaxMin();
         Comparator<Integer> comparator = Comparator.naturalOrder();
         List<Integer> listInt = new ArrayList<>(List.of(1, 7, 10, 5, 3));
-        int max = maxMin.max(listInt, comparator);
-        int expected = 10;
-        assertEquals(expected, max);
+        int min = maxMin.min(listInt, comparator);
+        int expected = 1;
+        assertEquals(expected, min);
+    }
+
+    @Test
+    public void whenStringsMin() {
+        MaxMin maxMin = new MaxMin();
+        Comparator<String> comparator = Comparator.naturalOrder();
+        List<String> listString = new ArrayList<>(List.of("England", "Andorra", "Denmark", "China", "Belgium"));
+        String country = maxMin.min(listString, comparator);
+        String expected = "Andorra";
+        assertEquals(expected, country);
     }
 }
