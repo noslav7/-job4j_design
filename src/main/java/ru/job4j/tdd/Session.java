91f5ab111cd6b7bfb3fe1370fbc2382d12d89c66
noslav7 <noslav7@gmail.com>
2022-05-20 08:37:24 +0500

    2. Что такое TDD? [#4918 #302636]

--- src/main/java/ru/job4j/tdd/Session.java
+++ src/main/java/ru/job4j/tdd/Session.java
@@ -1,7 +1,5 @@
 package ru.job4j.tdd;
 
-import ru.job4j.list.List;
-
 public interface Session {
-    void enjoy(Ticket ticket);
+
 }
--- src/main/java/ru/job4j/tdd/Ticket.java
+++ src/main/java/ru/job4j/tdd/Ticket.java
@@ -1,11 +1,5 @@
 package ru.job4j.tdd;
 
-import ru.job4j.list.List;
-
 public interface Ticket {
-    int obtain();
 
-    int use();
-
-    int sell();
 }
