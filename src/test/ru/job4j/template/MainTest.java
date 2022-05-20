f414434482e49b52671f9e20ae9c9a12a0d22fd6
noslav7 <noslav7@gmail.com>
2022-05-20 18:28:42 +0500

    3. Шаблонизатор. [#855]

--- src/test/ru/job4j/template/MainTest.java
+++ src/test/ru/job4j/template/MainTest.java
@@ -2,12 +2,11 @@ package ru.job4j.template;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import ru.job4j.map.Map;
 
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class  MainTest {
 
@@ -22,7 +21,7 @@ public class  MainTest {
     }
 
     @Ignore
-    @Test(expected = RuntimeException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void whenMapAlsoContainsOtherKeys() {
         Main main = new Main();
         String template = "I am a ${name}. Who are ${subject}";
@@ -30,5 +29,17 @@ public class  MainTest {
         map.put("Petr Arsentev", "you");
         map.put("Andrei Khynku", "they");
         String result = main.produce(template, map);
+    }
+
+    @Ignore
+    @Test
+    public void whenGenerationSuccessful() {
+        Main main = new Main();
+        String template = "I am a ${name}. Who are ${subject}";
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("Petr Arsentev", "you");
+        String result = main.produce(template, map);
+        String expected = "I am a Petr Arsentev. Who are you";
+        assertEquals(expected, result);
     }
 }
