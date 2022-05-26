180c5868ec69cad7514f01698f39b55ae19bf725
noslav7 <noslav7@gmail.com>
2022-05-26 08:06:35 +0500

    1. Отчеты. [#850 #304921]

--- src/test/ru/job4j/design/srp/ReportEngineTest.java
+++ src/test/ru/job4j/design/srp/ReportEngineTest.java
@@ -14,30 +14,15 @@ public class ReportEngineTest {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker1 = new Employee("Ivan", now, now, 100);
-        Employee worker2 = new Employee("Petr", now, now, 300);
-        Employee worker3 = new Employee("Sidor", now, now, 200);
         store.add(worker1);
-        store.add(worker2);
-        store.add(worker3);
         Report engine = new ReportEngine(store);
-        StringBuilder beforeHTML = new StringBuilder()
+        StringBuilder expect = new StringBuilder()
                 .append("Name; Hired; Fired; Salary;")
                 .append(System.lineSeparator())
                 .append(worker1.getName()).append(";")
                 .append(worker1.getHired()).append(";")
                 .append(worker1.getFired()).append(";")
-                .append(worker1.getSalary()).append(";")
-                .append(worker2.getName()).append(";")
-                .append(worker2.getHired()).append(";")
-                .append(worker2.getFired()).append(";")
-                .append(worker2.getSalary()).append(";")
-                .append(worker3.getName()).append(";")
-                .append(worker3.getHired()).append(";")
-                .append(worker3.getFired()).append(";")
-                .append(worker3.getSalary()).append(";")
-                .append(System.lineSeparator());
-
-        File file = new File()
+                .append(worker1.getSalary()).append(";");
 
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
