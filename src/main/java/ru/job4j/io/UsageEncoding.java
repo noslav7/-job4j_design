dfa445aede06f07d0a6db51c4723be44e5aa2a2a
noslav7 <noslav7@gmail.com>
2022-06-02 20:18:48 +0500

    1. Отчеты. [#850 #304921]

--- src/main/java/ru/job4j/io/UsageEncoding.java
+++ src/main/java/ru/job4j/io/UsageEncoding.java
@@ -2,6 +2,8 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UsageEncoding {
@@ -15,30 +17,45 @@ public class UsageEncoding {
         return builder.toString();
     }
 
-    public void writeDataInFile(String path, String data) {
-        try (PrintWriter pw = new PrintWriter(
-                new FileWriter(path, Charset.forName("UTF-8"), true))) {
-            pw.println(data);
+    public void writeDataInFile(String path, List<String> data) {
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, Charset.forName("UTF-8"), true))) {
+            data.forEach(pw::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String path = "./src/data/text.txt";
         UsageEncoding encoding = new UsageEncoding();
-        List<String> strings = List.of(
-                "Новая строка 1",
-                "Новая строка 2",
-                "Новая строка 3",
-                "Новая строка 4",
-                "Новая строка 5"
-        );
-        for (String str : strings) {
-            encoding.writeDataInFile(path, str);
-        }
+        List<String> strings = new ArrayList<>(List.of(
+                "Свободы сеятель пустынный",
+                "Я вышел рано, до звезды",
+                "Рукою чистой и безвинной в порабощенные бразды",
+                "Бросал живительное семя,",
+                "Но потерял я только время, благие мысли и труды..."
+        ));
+        encoding.writeDataInFile(path, strings);
         String s = encoding.readFile(path);
-        System.out.println("Данные из файла: ");
-        System.out.println(s);
+
+        while (!anyWordOrPhrase().equals("стоп")) {
+            Collections.shuffle(strings);
+            System.out.println(strings.get(0));
+        }
+        System.out.println("стоп");
+        while (!anyWordOrPhrase().equals("продолжить")) {
+            continue;
+        }
+        System.out.println("продолжить");
+        while (!anyWordOrPhrase().equals("закончить")) {
+            Collections.shuffle(strings);
+            System.out.println(strings.get(0));
+        }
+        System.out.println("закончить");
+    }
+
+    public static String anyWordOrPhrase() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
     }
 }
