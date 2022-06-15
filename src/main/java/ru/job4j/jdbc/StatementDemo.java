6d213df8da614c6606287e841aca81d7ea2cfc00
noslav7 <noslav7@gmail.com>
2022-06-15 09:22:50 +0500

    0.1. Statement [#379306]

--- src/main/java/ru/job4j/jdbc/StatementDemo.java
+++ src/main/java/ru/job4j/jdbc/StatementDemo.java
@@ -23,7 +23,7 @@ public class StatementDemo {
 
     private static Connection getConnection() throws Exception {
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String url = "jdbc:postgresql://localhost:5432/postgres";
         String login = "postgres";
         String password = "password";
         return DriverManager.getConnection(url, login, password);
--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,36 +1,115 @@
 package ru.job4j.jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.StringJoiner;
+
+import static java.sql.DriverManager.getConnection;
 
 public class TableEditor implements AutoCloseable {
     private Connection connection;
 
     private Properties properties;
 
-    public TableEditor(Properties properties) {
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
         this.properties = properties;
         initConnection();
     }
 
-    private void initConnection() {
-        connection = null;
+    private Connection initConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String login = "postgres";
+        String password = "password";
+        return DriverManager.getConnection(url, login, password);
     }
 
-    public void createTable(String tableName) {
+    public void createTable(String tableName) throws SQLException {
+        try (Connection connection = initConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "create table if not exists empty_table(%s, %s);",
+                        "id serial primary key",
+                        "name varchar(255)"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "empty_table"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void dropTable(String tableName) {
+    public void dropTable(String tableName) throws SQLException {
+        try (Connection connection = initConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "drop table (%s)", tableName
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "empty_table"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void addColumn(String tableName, String columnName, String type) {
+    public void addColumn(String tableName, String columnName, String type) throws SQLException {
+        try (Connection connection = initConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "alter table (%s)\nadd (%s, %s)",
+                        tableName, columnName, type
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "empty_table"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void dropColumn(String tableName, String columnName) {
+    public void dropColumn(String tableName, String columnName) throws SQLException {
+        try (Connection connection = initConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "alter table (%s)\ndrop column (%s)",
+                        tableName, columnName
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "empty_table"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void renameColumn(String tableName, String columnName, String newColumnName) {
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
+        try (Connection connection = initConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "alter table (%s)\nrename column (%s) to (%s)",
+                        tableName, columnName, newColumnName
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "empty_table"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
