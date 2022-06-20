702719ca6e986b80ed496f4737f6da6743393e7e
noslav7 <noslav7@gmail.com>
2022-06-20 10:13:42 +0500

    0.2. PrepareStatement [#379307]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -1,6 +1,9 @@
 package ru.job4j.spammer;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,21 +23,30 @@ public class ImportDB {
 
     public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
+        String rows;
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
-            /* rd.lines().forEach(...); */
+            rows = rd.lines().toString();
+        }
+        String[] splitInfo = rows.split(";");
+        for (int i = 0; i < splitInfo.length; i++) {
+            if (i % 2 == 0) {
+            users.add(new User(splitInfo[i], null));
+            } else {
+                users.get(i - 1).email = splitInfo[i];
+            }
         }
         return users;
     }
 
     public void save(List<User> users) throws ClassNotFoundException, SQLException {
-        Class.forName(cfg.getProperty("jdbc.driver"));
+        Class.forName(cfg.getProperty("jdbc.driver.ImportDB"));
         try (Connection cnt = DriverManager.getConnection(
-                cfg.getProperty("jdbc.url"),
-                cfg.getProperty("jdbc.username"),
-                cfg.getProperty("jdbc.password")
+                cfg.getProperty("jdbc:postgresql://localhost:5432/idea_db"),
+                cfg.getProperty("login"),
+                cfg.getProperty("password")
         )) {
             for (User user : users) {
-                try (PreparedStatement ps = cnt.prepareStatement("insert into users ...")) {
+                try (PreparedStatement ps = cnt.prepareStatement("insert into users(name, email) values (?, ?)")) {
                     ps.setString(1, user.name);
                     ps.setString(2, user.email);
                     ps.execute();
