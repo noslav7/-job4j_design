dd6557afc3a5a5c0a751e40c7f32988938cdd202
noslav7 <noslav7@gmail.com>
2022-06-23 09:32:40 +0500

    sql_exercises

--- src/main/java/ru/job4j/sql_exercises/easy/removing_repeating_line_3.sql
+++ src/main/java/ru/job4j/sql_exercises/easy/removing_repeating_line_3.sql
@@ -1,1 +1,7 @@
+INSERT INTO subjects VALUES(1, 'Math', 75, current_date);
+INSERT INTO subjects VALUES(2, 'Math', null, current_date);
+INSERT INTO subjects VALUES(3, 'Math', 75, current_date);
+INSERT INTO subjects VALUES(4, 'Math', 85, current_date);
+INSERT INTO subjects VALUES(5, 'Math', null, current_date);
+
 SELECT DISTINCT name, grade FROM products
