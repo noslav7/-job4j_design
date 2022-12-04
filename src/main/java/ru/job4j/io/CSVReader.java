427a8b6c093eb54ac9e8555596a82116a0f9b1fb
noslav7 <noslav7@gmail.com>
2022-12-04 17:40:59 +0500

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -2,14 +2,25 @@ package ru.job4j.io;
 
 import ru.job4j.io.ArgsName;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
+        File file = File.createTempFile("source.csv", ",");
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+            out.write(argsName.get(-filter=name));
+            out.write(argsName.get(-filter=age));
+        }
+        try (var scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                System.out.print(scanner.next());
+                System.out.print(",");
+            }
+        }
+
     }
 }
