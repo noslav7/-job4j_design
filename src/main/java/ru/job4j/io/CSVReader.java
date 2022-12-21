15513c5a042cf5762f95b09211cad36b63625dfa
noslav7 <noslav7@gmail.com>
2022-12-21 10:08:47 +0500

    CSVReader

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,27 +1,12 @@
 package ru.job4j.io;
 
-import ru.job4j.io.ArgsName;
-
-import java.io.*;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringJoiner;
-
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
-        Path path = Path.of("file.csv");
-        try (Scanner scanner = new Scanner(path).useDelimiter("; ")) {
-            while (scanner.hasNext()) {
-                String arg = scanner.next();
-                if (arg.equals(argsName.get("name"))) {
-                    System.out.print(arg);
-                    System.out.print(",");
-                } else if (arg.equals(argsName.get("age"))) {
-                    System.out.print(arg);
-                    System.out.println(",");
-                }
-            }
+        int numOfArgs = argsName.toString().split(",").length;
+        if (numOfArgs == 0 || numOfArgs > 5) {
+            throw new IllegalArgumentException("There should be from one to five arguments");
         }
+
+
     }
 }
