53fd6ea9e8c849534598cce42b4da84279bb9ae1
noslav7 <noslav7@gmail.com>
2022-12-21 11:09:53 +0500

    1. Представления [#504792]

--- src/sql/views/cumbersome_query.sql
+++ src/sql/views/cumbersome_query.sql
@@ -1,6 +1,6 @@
-create view show_students_with_2_or_more_books
-as select s.name as student, count(a.name), a.name as author from students as s
+create view show_authors_of_single_books_taken_by_students
+as select a.name as authors, s.name as students, b.name as books from students as s
 join orders o on s.id = o.student_id
 join books b on o.book_id = b.id
 join authors a on b.author_id = a.id
-group by (s.name, a.name) having count(a.name) >= 2;
+group by (s.name, a.name, b.name) having count(a.name) = 1;
