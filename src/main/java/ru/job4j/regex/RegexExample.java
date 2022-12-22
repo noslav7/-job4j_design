d1e8f301fd772546c7ff8a2e8f8012a8e1571e2c
noslav7 <noslav7@gmail.com>
2022-12-22 11:44:09 +0500

    RegexExample

---
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -0,0 +1,15 @@
+package ru.job4j.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("11");
+        String text = "111111";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+}
