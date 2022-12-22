b273d1b25df4ba1a6e568995fe0fa48a0f31b50f
noslav7 <noslav7@gmail.com>
2022-12-22 12:37:03 +0500

    1. Представления [#504792 #373186]

--- src/sql/views/cumbersome_query.sql
+++ src/sql/views/cumbersome_query.sql
@@ -1,4 +1,5 @@
-select s.name, a.name as authors from students as s
+create view show_students_and_authors
+as select s.name, a.name as authors from students as s
 join orders o on s.id = o.student_id
 join books b on o.book_id = b.id
 join authors a on b.author_id = a.id
