e02a16d11a27fb41f768d3520d30df55160782cc
noslav7 <noslav7@gmail.com>
2023-01-16 15:51:40 +0500

    9. Подзапросы [#504874 #381622]

--- src/sql/dbo/subqueries.sql
+++ src/sql/dbo/subqueries.sql
@@ -11,3 +11,20 @@ INSERT INTO customers VALUES(1, 'Manuel', 'Neuer', 36, 'Germany'),
                       (3, 'Luca', 'Modric', 38, 'Croatia'),
                       (4, 'Sadyo', 'Mane', 30, 'Senegal'),
                       (5, 'Lonel', 'Messi', 35, 'Argentina');
+
+select first_name, last_name from customers
+where age = (select min(age) from customers);
+
+CREATE TABLE orders(
+id serial primary key,
+amount int,
+customer_id int references customers(id)
+);
+
+INSERT INTO orders VALUES(1, 5, 1),
+                         (2, 4, 2),
+                         (3, 3, 3),
+                         (4, 4, 4);
+
+select first_name, last_name from customers
+where id NOT IN (select customer_id from orders);
