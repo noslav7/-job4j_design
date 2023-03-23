6c9e9cc8aef7b5ed7080c412e0d675e7d56e09ea
noslav7 <noslav7@gmail.com>
2023-03-23 11:22:55 +0500

    5.0. Буферизированные потоки [#504979 #403438]

--- src/main/java/ru/job4j/io/PrintUsage.java
+++ src/main/java/ru/job4j/io/PrintUsage.java
@@ -3,15 +3,11 @@ package ru.job4j.io;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 
 public class PrintUsage {
     public static void main(String[] args) {
-        try (PrintStream stream = new PrintStream(new FileOutputStream("C:/projects/job4j_design/src/data/print.txt"));
-             PrintWriter writer = new PrintWriter("C:/projects/job4j_design/src/data/write.txt")) {
+        try (PrintStream stream = new PrintStream(new FileOutputStream("data/print.txt"))) {
             stream.println("Из PrintStream в FileOutputStream");
-            stream.write("Новая строка".getBytes());
-            writer.println("Новое сообщение");
         } catch (IOException e) {
             e.printStackTrace();
         }
