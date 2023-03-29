7bbbca8719b5b5ba5279efa6953cc2c4acc41a00
noslav7 <noslav7@gmail.com>
2023-03-29 10:30:19 +0500

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,12 +1,30 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
 public class CSVReader {
     public static void handle(ArgsName argsName) throws Exception {
-        int numOfArgs = argsName.toString().split(",").length;
-        if (numOfArgs == 0 || numOfArgs > 5) {
-            throw new IllegalArgumentException("There should be from one to five arguments");
+        File file = File.createTempFile("file", ".csv");
+        Scanner scanner = new Scanner(file).useDelimiter(",");
+
+        while (scanner.hasNext()) {
+            System.out.print(scanner.next(argsName.get("name")));
+            System.out.print(" ");
+            System.out.println(scanner.next(argsName.get("age")));
+            scanner.nextLine();
         }
 
+    }
 
+    public static void main(String[] args) throws Exception {
+        if (args.length != 5) {
+            throw new IllegalArgumentException("Number of arguments should be 5");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        handle(argsName);
     }
 }
