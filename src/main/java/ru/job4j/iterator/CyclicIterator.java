de88ecfa1d5affbb0c0e1a565eac105912f8a960
noslav7 <noslav7@gmail.com>
2023-08-16 18:47:25 +0500

    6. Циклический итератор [#505082 #437246]

--- src/main/java/ru/job4j/iterator/CyclicIterator.java
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -15,24 +15,19 @@ public class CyclicIterator<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
-        boolean hasNext = false;
-        if (index + 1 < data.size() && !data.isEmpty()) {
-            hasNext = true;
-        } else if (index + 1 == data.size() && !data.isEmpty()) {
-            hasNext = true;
-        }
-        return hasNext;
+        return index <= data.size() && !data.isEmpty();
     }
 
     @Override
     public T next() {
         T element = null;
-        if (index + 1 < data.size()) {
-            element = data.get(index);
-            index = index + 1;
-        } else if (index + 1 == data.size()) {
-            element = data.get(data.size() - 1);
+        if (index == data.size() && !data.isEmpty()) {
+            element = data.get(0);
             index = 0;
+        }
+        if (index < data.size() && !data.isEmpty()) {
+            element = data.get(index);
+            index++;
         }
         if (element != null) {
             return element;
