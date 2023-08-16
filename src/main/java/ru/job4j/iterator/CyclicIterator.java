6a7301f9f03a3eb5232ae7b6fd8967d7230c6d39
noslav7 <noslav7@gmail.com>
2023-08-16 10:37:40 +0500

    6. Циклический итератор [#505082]

---
+++ src/main/java/ru/job4j/iterator/CyclicIterator.java
@@ -0,0 +1,25 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class CyclicIterator<T> implements Iterator<T>{
+
+    private List<T> data;
+    /* здесь разместите поля класса, если они будут нужны  */
+
+    public CyclicIterator(List<T> data) {
+        /* код конструктора */
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+}
